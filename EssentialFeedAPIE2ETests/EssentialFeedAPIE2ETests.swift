c316fa4bf0840bed457cc976ba52ca7ffbfa3261
Saravanakumar S <ssarawanakumar@gmail.com>
2021-02-12 20:42:51 +0530

    Add e2e api in a separate test target

---
+++ EssentialFeedAPIE2ETests/EssentialFeedAPIE2ETests.swift
@@ -0,0 +1,85 @@
+//
+//  EssentialFeedAPIE2ETests.swift
+//  EssentialFeedAPIE2ETests
+//
+//  Created by Saravanakumar S on 12/02/21.
+//
+
+import XCTest
+import EssentialFeed
+
+class EssentialFeedAPIE2ETests: XCTestCase {
+
+    func test_e2eServerGETRequest_MatchesFeedWithTestAccount() {
+        let testURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(client: client, url: testURL)
+        var receivedResult: LoadFeedResult?
+        
+        let exp = expectation(description: "Wait for api call to return")
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 10.0)
+        
+        switch receivedResult {
+        case let .success(items)?:
+            XCTAssertEqual(items[0], getItem(at: 0))
+            XCTAssertEqual(items[1], getItem(at: 1))
+            XCTAssertEqual(items[2], getItem(at: 2))
+            XCTAssertEqual(items[3], getItem(at: 3))
+            XCTAssertEqual(items[4], getItem(at: 4))
+            XCTAssertEqual(items[5], getItem(at: 5))
+            XCTAssertEqual(items[6], getItem(at: 6))
+            XCTAssertEqual(items[7], getItem(at: 7))
+        case let .failure(error)?:
+            XCTFail("Expected successful result but got error \(error)")
+        default:
+            XCTFail("Expected to pass")
+        }
+    }
+
+    func getItem(at index: Int) -> FeedItem {
+        return FeedItem(id: getId(at: index), description: getDesc(at: index), location: getLocation(at: index), imageURL: getImageURL(at: index))
+    }
+    
+    func getId(at i: Int) -> UUID {
+        return UUID(uuidString: [
+            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+            "F79BD7F8-063F-46E2-8147-A67635C3BB01"
+        ][i])!
+    }
+    
+    func getDesc(at index: Int) -> String? {
+        return ["Description 1",
+                nil,
+                "Description 3",
+                nil,
+                "Description 5",
+                "Description 6",
+                "Description 7",
+                "Description 8"][index]
+    }
+    
+    func getLocation(at index: Int) -> String? {
+        return ["Location 1",
+                "Location 2",
+                nil,
+                nil,
+                "Location 5",
+                "Location 6",
+                "Location 7",
+                "Location 8"][index]
+    }
+    
+    func getImageURL(at index: Int) -> URL {
+        return URL(string: "https://url-\(index+1).com")!
+    }
+}
