ff8e23688d2ce36a2c3a1cc1fe52abfa3bd446e6
Saravanakumar S <saravanakumars@equalexperts.com>
2021-02-28 14:17:56 +0530

    Add Feetstoreinto separate file

---
+++ EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Saravanakumar S on 28/02/21.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?)->Void
+    typealias InsertionCompletion = (Error?)->Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
