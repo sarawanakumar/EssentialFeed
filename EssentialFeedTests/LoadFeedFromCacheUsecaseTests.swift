84a20152b5791cff4ebc11a5ff3e085c9ff992af
Saravanakumar S <saravanakumars@equalexperts.com>
2021-03-02 13:38:36 +0530

    Load command fails on retrieval error

--- EssentialFeedTests/LoadFeedFromCacheUsecaseTests.swift
+++ EssentialFeedTests/LoadFeedFromCacheUsecaseTests.swift
@@ -19,9 +19,25 @@ class LoadFeedFromCacheUsecaseTests: XCTestCase {
     func test_load_requestCacheRetrieval() {
         let (store, sut) = makeSut()
         
-        sut.load()
+        sut.load() { _ in }
          
         XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_load_failsOnRetrievalError() {
+        let (store, sut) = makeSut()
+        let retrievalError = anyNSError()
+        var receivedError: Error?
+        let exp = expectation(description: "wait")
+        sut.load() { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        
+        store.completeRetrieval(with: retrievalError)
+        wait(for: [exp], timeout: 1.0)
+         
+        XCTAssertEqual(receivedError as NSError?, retrievalError)
     }
     
     private func makeSut(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (store: FeedStoreSpy, sut: LocalFeedLoader) {
@@ -31,5 +47,9 @@ class LoadFeedFromCacheUsecaseTests: XCTestCase {
         trackForMemoryLeaks(instance: store, file: file, line: line)
         trackForMemoryLeaks(instance: sut, file: file, line: line)
         return (store, sut)
+    }
+    
+    func anyNSError() -> NSError{
+        return NSError(domain: "An Error ", code: 0)
     }
 }
