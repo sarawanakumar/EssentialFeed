e28711ac6f92430bd145f0192bd63ae72e54f517
Saravanakumar S <saravanakumars@equalexperts.com>
2021-03-20 02:17:35 +0530

    Add helper method to provide, documentation ,context and clarify test setup and teardown intern t regarding side effect

--- EssentialFeedTests/FeedStore/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedStore/CodableFeedStoreTests.swift
@@ -62,13 +62,13 @@ class CodableFeedStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        setupEmptyStoreState()
     }
     
     override func tearDown() {
         super.tearDown()
         
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        undoStoreSideEffects()
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -140,5 +140,17 @@ class CodableFeedStoreTests: XCTestCase {
     
     func testSpecificStoreURL() -> URL {
         FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
     }
 }
