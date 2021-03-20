334bedfc183e3b0d9292706587a0d126d63ae81a
Saravanakumar S <saravanakumars@equalexperts.com>
2021-03-20 18:38:02 +0530

    multiple Retrieves should return the same inserted value always

--- EssentialFeedTests/FeedStore/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedStore/CodableFeedStoreTests.swift
@@ -130,6 +130,34 @@ class CodableFeedStoreTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_retrieveTwiceAfterInsertingValue_deliversTheSameInsertedValue() {
+        let sut = makeSUT()
+        let feed = uniqueImageFeed().locals
+        let timestamp = Date()
+        
+        let exp = expectation(description: "wait for completion block")
+        sut.insert(feed, timestamp: timestamp) { insertionError in
+            XCTAssertNil(insertionError, "insertion should be success")
+            sut.retrieve { firstResult in
+                sut.retrieve { secondResult in
+                    switch (firstResult, secondResult) {
+                    case let (.found(feed: firstFound, timestamp: firstTs), .found(feed: secondFound, timestamp: secondTs)):
+                        XCTAssertEqual(firstFound, feed)
+                        XCTAssertEqual(firstTs, timestamp)
+                        
+                        XCTAssertEqual(secondFound, feed)
+                        XCTAssertEqual(secondTs, timestamp)
+                    default:
+                        XCTFail("expected to return Same inserted feed \(feed) for on both retrieves but got \(firstResult) and then \(secondResult)")
+                    }
+                    exp.fulfill()
+                }
+            }
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     func makeSUT(file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
         let sut = CodableFeedStore(storeURL: testSpecificStoreURL())
         
